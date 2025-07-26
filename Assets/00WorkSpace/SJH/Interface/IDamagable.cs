bac3e8d5e4c64fc3df859f4a137639d09cff0e80
Clean18 <cleanage95@gmail.com>
2025-07-26 13:48:20 +0900

    [Feat] : IDamagable 인터페이스 추가

---
+++ Assets/00WorkSpace/SJH/Interface/IDamagable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public interface IDamagable
+{
+    PokemonData DefenderPokeData { get; }
+    PokemonStat DefenderPokeStat { get; }
+    int DefenderLevel { get; }
+    int DefenderMaxHp { get; }
+    int DefenderCurrentHp { get; }
+    public void TakeDamage(int value);
+}
