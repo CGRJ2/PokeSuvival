e285f0351c88875990015433ab1024a866e2a42c
Clean18 <cleanage95@gmail.com>
2025-07-30 19:13:57 +0900

    [Fix] : 아이템박스에서 드랍되는 아이템 동기화

--- Assets/00WorkSpace/NTJ/Scripts/ItemDatabase.cs
+++ Assets/00WorkSpace/NTJ/Scripts/ItemDatabase.cs
@@ -1,4 +1,4 @@
-using NTJ;
+﻿using NTJ;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,27 +8,4 @@ using UnityEngine;
 public class ItemDatabase : ScriptableObject
 {
     public ItemData[] items;
-
-    private Dictionary<int, ItemData> itemDict;
-
-    private static ItemDatabase _instance;
-    public static ItemDatabase Instance
-    {
-        get
-        {
-            if (_instance == null)
-                _instance = Resources.Load<ItemDatabase>("ItemDatabase");
-            return _instance;
-        }
-    }
-
-    private void OnEnable()
-    {
-        itemDict = items.ToDictionary(i => i.id);
-    }
-
-    public ItemData GetItemById(int id)
-    {
-        return itemDict != null && itemDict.TryGetValue(id, out var data) ? data : null;
-    }
 }
