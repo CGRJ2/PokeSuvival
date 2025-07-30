efee8bc86c2d8442eee8d48ada8c0015ef3ffa93
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-07-30 19:53:52 +0900

    [Chore] UIManager 프리펩 업데이트

--- Assets/00WorkSpace/CJM/Scripts/UI/InGameGroup/Panel_SkillSlots.cs
+++ Assets/00WorkSpace/CJM/Scripts/UI/InGameGroup/Panel_SkillSlots.cs
@@ -4,19 +4,9 @@ public class Panel_SkillSlots : MonoBehaviour
 {
     SkillSlotView[] skillSlots;
 
-    float test = 0;
-
-
     public void Init()
     {
         skillSlots = GetComponentsInChildren<SkillSlotView>();
-
-    }
-
-
-    private void Update()
-    {
-        Debug.LogWarning(test);
     }
 
     public void UpdateSkillSlotsView(PlayerModel model)
@@ -33,16 +23,12 @@ public class Panel_SkillSlots : MonoBehaviour
             skillSlots[i].actived = true;
         }
 
-
         for (int i = 0; i < skillSlots.Length; i++)
         {
             // 해당 슬롯에 넣을 스킬이 있다면
             if (skillSlots[i].actived)
             {
                 skillSlots[i].UpdateSlotView(pokemonData.Skills[i]);
-                //model.SkillCooldownDic[(SkillSlot)i]
-                //Debug.Log($"쿨타임 받아오기 {model.SkillCooldownDic[(SkillSlot)i]}");
-                test = model.SkillCooldownDic[(SkillSlot)i];
             }
             // 해당 슬롯에 넣을 스킬이 없다면
             else
@@ -50,8 +36,6 @@ public class Panel_SkillSlots : MonoBehaviour
                 skillSlots[i].BlockSlotView();
             }
         }
-
-        //float cool_01 = model.SkillCooldownDic[SkillSlot.Skill1];
     }
 
 
