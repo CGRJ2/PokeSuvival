7d4a1a9f7ea41b58f94df3afea77cad30b03d35e
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-08-02 16:32:34 +0900

    [Fix] 게스트 로그인 후 퀵매치가 안되던 오류 수정

--- Assets/00WorkSpace/CJM/Scripts/UI/LobbyGroup/Default/Panel_SelectedPokemonView.cs
+++ Assets/00WorkSpace/CJM/Scripts/UI/LobbyGroup/Default/Panel_SelectedPokemonView.cs
@@ -22,58 +22,19 @@ public class Panel_SelectedPokemonView : MonoBehaviour
     {
         Debug.Log("포켓몬 인포 패널 업데이트");
 
-        // 로그인을 한 유저라면
-        if (BackendManager.Auth.CurrentUser != null)
+        // 커스텀 프로퍼티로 스타팅 포켓몬 설정 여부 판단
+        if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("StartingPokemon"))
         {
-            BackendManager.Instance.LoadUserDataFromDB((userData) =>
+            // 스타팅 포켓몬이 설정되어 있다면 UI창 업데이트
+            if (!string.IsNullOrEmpty((string)PhotonNetwork.LocalPlayer.CustomProperties["StartingPokemon"]))
             {
-                // 스타팅 포켓몬이 설정되어 있다면 UI창 업데이트
-                if (userData.startingPokemonName != "None")
-                {
-                    string pokemonDataSO_Name = (string)PhotonNetwork.LocalPlayer.CustomProperties["StartingPokemon"];
-                    PokemonData selectedPokemonData = Define.GetPokeData(pokemonDataSO_Name);
+                string pokemonDataSO_Name = (string)PhotonNetwork.LocalPlayer.CustomProperties["StartingPokemon"];
+                PokemonData selectedPokemonData = Define.GetPokeData(pokemonDataSO_Name);
 
-                    tmp_Name.text = selectedPokemonData.PokeName;
-                    image_Sprite.sprite = selectedPokemonData.PokemonInfoSprite;
-                }
-                // 설정이 안되어 있다면
-                else
-                {
-                    // 포켓몬 이름 text에 기본값 넣어주기
-                    tmp_Name.text = "스타팅 포켓몬을\r\n설정해주세요";
-
-                    // 물음표 표시로 바꿔주기
-                    image_Sprite.sprite = sprite_noneSettedImage;
-                }
-            });
-        }
-
-        // 게스트 로그인을 한 유저라면
-        else
-        {
-            // 포톤 로컬 유저 커스텀 프로퍼티로 스타팅 포켓몬 설정 여부 판단
-            if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("StartingPokemon"))
-            {
-                // 스타팅 포켓몬이 설정되어 있다면 UI창 업데이트
-                if (!string.IsNullOrEmpty((string)PhotonNetwork.LocalPlayer.CustomProperties["StartingPokemon"]))
-                {
-                    string pokemonDataSO_Name = (string)PhotonNetwork.LocalPlayer.CustomProperties["StartingPokemon"];
-                    PokemonData selectedPokemonData = Define.GetPokeData(pokemonDataSO_Name);
-
-                    tmp_Name.text = selectedPokemonData.PokeName;
-                    image_Sprite.sprite = selectedPokemonData.PokemonInfoSprite;
-                }
-                // 스타팅 포켓몬 프로퍼티는 있는데 설정이 안되어 있다면
-                else
-                {
-                    // 포켓몬 이름 text에 기본값 넣어주기
-                    tmp_Name.text = "스타팅 포켓몬을\r\n설정해주세요";
-
-                    // 물음표 표시로 바꿔주기
-                    image_Sprite.sprite = sprite_noneSettedImage;
-                }
+                tmp_Name.text = selectedPokemonData.PokeName;
+                image_Sprite.sprite = selectedPokemonData.PokemonInfoSprite;
             }
-            // 스타팅 포켓몬 프로퍼티 자체가 없다면
+            // 스타팅 포켓몬 프로퍼티는 있는데 설정이 안되어 있다면
             else
             {
                 // 포켓몬 이름 text에 기본값 넣어주기
@@ -82,6 +43,15 @@ public class Panel_SelectedPokemonView : MonoBehaviour
                 // 물음표 표시로 바꿔주기
                 image_Sprite.sprite = sprite_noneSettedImage;
             }
+        }
+        // 스타팅 포켓몬 프로퍼티 자체가 없다면
+        else
+        {
+            // 포켓몬 이름 text에 기본값 넣어주기
+            tmp_Name.text = "스타팅 포켓몬을\r\n설정해주세요";
+
+            // 물음표 표시로 바꿔주기
+            image_Sprite.sprite = sprite_noneSettedImage;
         }
     }
 
