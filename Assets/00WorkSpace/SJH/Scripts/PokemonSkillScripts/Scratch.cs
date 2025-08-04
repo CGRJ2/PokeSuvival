0c6a3480be457e111a04f66d97e50e84b55b4354
Clean18 <cleanage95@gmail.com>
2025-08-04 14:35:05 +0900

    [Feat] PokemonData 필드변수 Size 추가 및 SO들 값 수정

--- Assets/00WorkSpace/SJH/Scripts/PokemonSkillScripts/Scratch.cs
+++ Assets/00WorkSpace/SJH/Scripts/PokemonSkillScripts/Scratch.cs
@@ -5,7 +5,7 @@ public class Scratch : IAttack
 {
 	public void Attack(Transform attacker, Vector2 attackDir, BattleDataTable attackerData, PokemonSkill skill)
 	{
-		var enemies = Physics2D.OverlapCircleAll((Vector2)attacker.position, skill.Range);
+		var enemies = Physics2D.OverlapCircleAll((Vector2)attacker.position, skill.Range + attackerData.PokeData.PokeSize);
 		if (enemies.Length <= 0) return;
 
 		foreach (var enemy in enemies)
@@ -18,15 +18,6 @@ public class Scratch : IAttack
 				var iD = enemy.GetComponent<IDamagable>();
 				if (iD == null) continue;
 				iD.TakeDamage(attackerData, skill);
-				//var pv = enemy.GetComponent<PhotonView>();
-				//if (iD == null || pv == null) continue;
-				//var defenderData = iD.BattleData;
-				//if (defenderData.CurrentHp <= 0) continue;
-				//int damage = PokeUtils.CalculateDamage(attackerData, defenderData, skill);
-				////pv.RPC("RPC_TakeDamage", pv.Owner, damage);
-				//iD.TakeDamage(damage);
-				//PlayerManager.Instance?.ShowDamageText(pv.gameObject.transform, damage, Color.white);
-				//Debug.Log($"Lv.{attackerData.Level} {attackerData.PokeData.PokeName} 이/가 Lv.{defenderData.Level} {defenderData.PokeData.PokeName} 을/를 {skill.SkillName} 공격!");
 			}
 		}
 		Debug.Log($"{skill.SkillName} 공격!");
