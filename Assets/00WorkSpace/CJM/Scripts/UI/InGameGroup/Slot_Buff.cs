d0a2e0980b40046bdfe86981e066ebbc77c76682
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-08-07 12:59:27 +0900

    [Chore] 지닌 물건 인게임 UI 버프상태 업데이트

--- Assets/00WorkSpace/CJM/Scripts/UI/InGameGroup/Slot_Buff.cs
+++ Assets/00WorkSpace/CJM/Scripts/UI/InGameGroup/Slot_Buff.cs
@@ -24,27 +24,44 @@ public class Slot_Buff : MonoBehaviour
 
     public void UpdateBuffSlot(Sprite sprite, float duration)
     {
-        gameObject.SetActive(true);
-        image_Buff.sprite = sprite;
+        if (duration <= -98)
+        {
+            image_Buff.sprite = sprite;
+            this.duration = duration;
+            tmp_Duration.gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+            tmp_Duration.gameObject.SetActive(true);
 
-        startTime = Time.time;
-        this.duration = duration;
+            image_Buff.sprite = sprite;
+
+            startTime = Time.time;
+            this.duration = duration;
+        }
     }
 
     public void UpdateView()
     {
-        duration -= Time.deltaTime;
-
-        // 버프 지속시간 종료 시
-        if (duration < 0)
+        if (duration <= -98)
         {
-            gameObject.SetActive(false);
+
         }
-        // 버프 지속시간 동안
         else
         {
-            tmp_Duration.text = $"{Mathf.Clamp((int)duration, 0, 999)}";
-        }
+            duration -= Time.deltaTime;
 
+            // 버프 지속시간 종료 시
+            if (duration < 0)
+            {
+                gameObject.SetActive(false);
+            }
+            // 버프 지속시간 동안
+            else
+            {
+                tmp_Duration.text = $"{Mathf.Clamp((int)duration, 0, 999)}";
+            }
+        }
     }
 }
