7ae02da1c2b0723e5203c2a0dc80fdb1fd861486
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-08-08 20:45:13 +0900

    스킬 추가

--- Assets/00WorkSpace/SJH/Scripts/PokemonSkillScripts/SkillStrategyAttack.cs
+++ Assets/00WorkSpace/SJH/Scripts/PokemonSkillScripts/SkillStrategyAttack.cs
@@ -22,6 +22,7 @@ public class SkillStrategyAttack : IAttack
 			// 노말
 			case "할퀴기": return new Scratch();
 			case "몸통박치기": return new Tackle();
+			case "트라이어택": return new TriAttack();
 			// 풀
 			case "잎날가르기": return new RazorLeaf();
 			case "덩굴채찍": return new VineWhip();
@@ -33,7 +34,24 @@ public class SkillStrategyAttack : IAttack
 			case "도깨비불": return new WillOWisp();
 			case "불대문자": return new FireBlast();
 			case "쾌청": return new SunnyDay();
-			default: return null;
+
+            // 전기
+            case "전기쇼크": return new Thundershock();
+
+            // 땅
+            case "진흙뿌리기": return new MudSlap();
+
+            // 드래곤
+            case "용의숨결": return new DragonBreath();
+            // 고스트
+            case "섀도볼": return new ShadowBall();
+            // 에스퍼
+            case "사이코키네시스": return new Psychic();
+
+            // 독
+            case "오물폭탄": return new SludgeBomb();
+
+            default: return null;
 		}
 	}
 }
